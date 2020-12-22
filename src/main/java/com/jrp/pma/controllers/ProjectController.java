4c3eee4238a896c860d8d14629bf2d06c8d2c654
kapil_gaur <kapilgaur37@gmail.com>
2020-12-22 12:04:37 +0530

    Commit by Kapil Gaur on 22-12-2020

--- src/main/java/com/jrp/pma/controllers/ProjectController.java
+++ src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -1,5 +1,7 @@
 package com.jrp.pma.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.entities.Employee;
 import com.jrp.pma.entities.Project;
 
 @Controller
@@ -16,6 +19,13 @@ public class ProjectController {
 	
 	@Autowired
 	ProjectRepository proRepo;
+	
+	@GetMapping
+	public String displayProjects(Model model) {
+		List<Project> projects = proRepo.findAll();
+		model.addAttribute("projects", projects);
+		return "/projects/list-projects";
+	}
 	
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
