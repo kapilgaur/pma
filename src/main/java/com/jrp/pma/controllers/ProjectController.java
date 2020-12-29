58e6f8555ca21763739674c877384aa114223f94
kapil_gaur <kapilgaur37@gmail.com>
2020-12-29 17:48:06 +0530

    Commit by Kapil Gaur on 29-12-2020

--- src/main/java/com/jrp/pma/controllers/ProjectController.java
+++ src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.entities.Employee;
 import com.jrp.pma.entities.Project;
@@ -19,6 +21,9 @@ public class ProjectController {
 	
 	@Autowired
 	ProjectRepository proRepo;
+	
+	@Autowired
+	EmployeeRepository empRepo;
 	
 	@GetMapping
 	public String displayProjects(Model model) {
@@ -31,7 +36,9 @@ public class ProjectController {
 	public String displayProjectForm(Model model) {
 		
 		Project aProject = new Project();
+		List<Employee> employees = empRepo.findAll();
 		model.addAttribute("project", aProject);
+		model.addAttribute("allEmployees", employees);
 		
 		return "projects/new-project";
 	}
@@ -41,8 +48,8 @@ public class ProjectController {
 		
 		// this should handel saving to the database...
 				proRepo.save(project);
-
-		
+				
+				
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/projects/new ";
 	}
--- src/main/java/com/jrp/pma/entities/Employee.java
+++ src/main/java/com/jrp/pma/entities/Employee.java
@@ -1,11 +1,16 @@
 package com.jrp.pma.entities;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Employee {
@@ -18,15 +23,30 @@ public class Employee {
 	private String lastName;
 	private String email;
 	
-	@ManyToOne
-	@JoinColumn(name="project_id")
-	private Project theProject;
+	@ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST}, fetch = FetchType.LAZY)
+	@JoinTable(name="project_employee",
+	   joinColumns=@JoinColumn(name="employee_id"),
+	   inverseJoinColumns=@JoinColumn(name="project_id")
+	)
+	private List<Project> projects;
 	
 	
 	public Employee() {
 		
 	}
 	
+	
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+
+
 	public Employee(String firstName, String lastName, String email) {
 		super();
 		this.firstName = firstName;
